0f18368adb5a1d089e43b61170f281b86c23b442
William Knauss <william.knauss@kingdom.com>
2017-09-18 11:59:55 -0400

    only show jumbotron if there is a jumbotron to show

--- theme/dark/templates/home.php
+++ theme/dark/templates/home.php
@@ -1,30 +1,58 @@
 <?php echo $header; ?>
 <!-- JUMBOTRON
 =================================-->
-<div class="jumbotron text-center">
-	<?php if ($page['enableSlider'] == 1) { ?>
-    <div id="carousel" class="carousel slide " data-ride="carousel">
-        <?php getCarousel($page['pageID']); ?>
-      <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
-    <?php } ?>
-		<?php if ($page['enableJumbotron'] == 1) { ?>
-	    <div class="container content-padding">
-	      <div class="row">
-					<div class="col-md-12">
-						<?php echo $page['jumbotronHTML']; ?>
-					</div>
+<?php
+$enabled   = false;
+$slider    = false;
+$jumbotron = false;
+
+if ($page['enableSlider'] == 1) {
+    $enabled = true;
+    $slider  = true;
+}
+
+if ($page['enableJumbotron'] == 1) {
+    $enabled   = true;
+    $jumbotron = true;
+}
+
+if ($enabled == true) {
+    echo '<div class="jumbotron text-center">';
+
+    if ($slider == true) {
+        echo
+        '
+<div id="carousel" class="carousel slide " data-ride="carousel">
+  ' . getCarousel($page['pageID']) . '
+  <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+
+  <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+';
+    }
+
+    if ($jumbotron == true) {
+        echo
+            '
+    <div class="container content-padding">
+      <div class="row">
+        <div class="col-md-12">
+          ' . $page['jumbotronHTML'] . '
         </div>
       </div>
-		<?php } ?>
-</div>
+    </div>
+  ';
+    }
+
+    echo '</div>';
+}
+?>
 
 
 
